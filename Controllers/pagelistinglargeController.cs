28d0d79997321f20c697d629e8c5e31e7e87ae7b
htmc-1707 <profile.hc.hotro@gmail.com>
2025-01-03 18:49:31 +0700

    Mô tả

---
+++ Controllers/pagelistinglargeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoangTruongManhCuong_2122110167.Controllers
+{
+    public class pagelistinglargeController : Controller
+    {
+        // GET: pagelistinglarge
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
